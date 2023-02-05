980df7a146421bd75f8a156ce3c1204b16278048
thanhdaonguyen <110709579+thanhdaonguyen@users.noreply.github.com>
2023-02-05 20:32:42 +0700

    Initial Commit

---
+++ SDL project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL project
+//
+//  Created by Thành Đạo Nguyễn on 05/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
